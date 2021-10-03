556325f807f9eff5cfdb2ec78ae107aa4fbf72eb
MohammadAl-khatib <alk20489@gmail.com>
2021-10-03 22:32:40 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_PORT}/data`).then((res) =>
+      this.setState({
+        data: res.data,
+      })
+    );
+  };
+
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
